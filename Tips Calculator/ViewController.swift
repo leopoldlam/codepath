c2f272d34a15987dd0770ba15f06253b720581c3
leopoldlam <leolam@Tien-Phans-MacBook-Air.local>
2015-04-13 17:15:00 -0700

    added some optional features

--- Tips Calculator/ViewController.swift
+++ Tips Calculator/ViewController.swift
@@ -12,16 +12,30 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var totalTextLabel: UILabel!
     @IBOutlet weak var billAmountField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
-    override func viewDidLoad() {
+    @IBOutlet weak var partyView: UIView!
+
+	@IBOutlet weak var totalForTwoLabel: UILabel!
+
+	@IBOutlet weak var totalForThreeLabel: UILabel!
+	
+	@IBOutlet weak var totalForFourLabel: UILabel!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
-        
+		totalForTwoLabel.text = "$0.00"
+		totalForThreeLabel.text = "$0.00"
+		totalForFourLabel.text = "$0.00"
+
+        billAmountField.becomeFirstResponder()
 
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -70,31 +84,75 @@ class ViewController: UIViewController {
         defaults.setObject(tipControl.selectedSegmentIndex, forKey: "defaultSelection")
         
 
-        
-        
-        
+        if billAmountField.text.isEmpty {
+            UIView.animateWithDuration(0.3, animations: {
+                self.partyView.alpha = 0
+                }
+            )
+        }
+        else
+        {
+            UIView.animateWithDuration(0.3, animations: {
+                self.partyView.alpha = 1
+                }
+            )
+        }
+		
+		var isDarkTheme = defaults.boolForKey("isDarkTheme")
+		if (isDarkTheme)
+		{
+			self.view.backgroundColor = UIColor.lightGrayColor()
+		}
+		else
+		{
+			self.view.backgroundColor = UIColor.whiteColor()
+		}
+		
         println("view will appear")
         
         defaults.synchronize()
     }
-    
+	func roundUpCent(totalAmount : Double, partyNumber : Double) -> Double
+	{
+		return (ceil((totalAmount / partyNumber) * 100) / 100)
+	}
+	
+	
+	func onValueChange()
+	{
+		
+		var billAmount = billAmountField.text._bridgeToObjectiveC().doubleValue
+		let _tipTitle : String = tipControl.titleForSegmentAtIndex(tipControl.selectedSegmentIndex)!
+		let tipTitle = _tipTitle.substringToIndex(_tipTitle.endIndex.predecessor())
+		var tipPercent  = NSNumberFormatter().numberFromString(tipTitle)!.doubleValue / 100
+		var tip = billAmount * tipPercent
+		var totalAmount = billAmount + tip
+		
+		var formatter = NSNumberFormatter()
+		formatter.numberStyle = .CurrencyStyle
+		formatter.locale = NSLocale.autoupdatingCurrentLocale()
+	
+		
+		tipLabel.text = formatter.stringFromNumber(tip)
+		totalLabel.text = formatter.stringFromNumber(totalAmount)
+		
+		totalForTwoLabel.text = formatter.stringFromNumber(roundUpCent(totalAmount, partyNumber: 2))
+		totalForThreeLabel.text = formatter.stringFromNumber(roundUpCent(totalAmount, partyNumber: 3))
+		totalForFourLabel.text = formatter.stringFromNumber( roundUpCent(totalAmount, partyNumber: 4))
+		
+	}
+	
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
         println("view did appear")
-        var billAmount = billAmountField.text._bridgeToObjectiveC().doubleValue
-        let _tipTitle : String = tipControl.titleForSegmentAtIndex(tipControl.selectedSegmentIndex)!
-        let tipTitle = _tipTitle.substringToIndex(_tipTitle.endIndex.predecessor())
-        var tipPercent  = NSNumberFormatter().numberFromString(tipTitle)!.doubleValue / 100
-        var tip = billAmount * tipPercent
-        var totalAmount = billAmount + tip
-        
-        tipLabel.text = String(format: "%.2f", tip)
-        totalLabel.text = String(format: "%.2f", totalAmount)
+		onValueChange()
     }
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
+		
+		
         println("view will disappear")
     }
     
@@ -106,17 +164,38 @@ class ViewController: UIViewController {
         println("view did disappear")
     }
 
-    @IBAction func onEditingChanged(sender: AnyObject) {
- 
-        var billAmount = billAmountField.text._bridgeToObjectiveC().doubleValue
-        let _tipTitle : String = tipControl.titleForSegmentAtIndex(tipControl.selectedSegmentIndex)!
-        let tipTitle = _tipTitle.substringToIndex(_tipTitle.endIndex.predecessor())
-        var tipPercent  = NSNumberFormatter().numberFromString(tipTitle)!.doubleValue / 100
-        var tip = billAmount * tipPercent
-        var totalAmount = billAmount + tip
+    @IBAction func onEditingDidEnd(sender: AnyObject) {
         
-        tipLabel.text = String(format: "%.2f", tip)
-        totalLabel.text = String(format: "%.2f", totalAmount)
+        if billAmountField.text.isEmpty {
+            UIView.animateWithDuration(0.3, animations: {
+                self.partyView.alpha = 0
+                }
+            )
+        }
+        else
+        {
+            UIView.animateWithDuration(0.3, animations: {
+                self.partyView.alpha = 1
+                }
+            )
+        }
+    }
+    @IBAction func onEditingChanged(sender: AnyObject) {
+		
+		onValueChange()
+ 
+//        var billAmount = billAmountField.text._bridgeToObjectiveC().doubleValue
+//        let _tipTitle : String = tipControl.titleForSegmentAtIndex(tipControl.selectedSegmentIndex)!
+//        let tipTitle = _tipTitle.substringToIndex(_tipTitle.endIndex.predecessor())
+//        var tipPercent  = NSNumberFormatter().numberFromString(tipTitle)!.doubleValue / 100
+//        var tip = billAmount * tipPercent
+//        var totalAmount = billAmount + tip
+//        
+//        tipLabel.text = String(format: "%.2f", tip)
+//        totalLabel.text = String(format: "%.2f", totalAmount)
+//        
+//        totalForTwoLabel.text = String(format: "%.2f", totalAmount * 2)
+
         
     }
 
